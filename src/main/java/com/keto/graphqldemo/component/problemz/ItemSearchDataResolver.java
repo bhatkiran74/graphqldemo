ba426267427a16239ffa449682472fc0cda52342
kiransing <bhatkiran74@gmail.com>
2026-08-11 01:33:37 +0530

    feat: implemented searchItems to ItemSearchDataResolver class

--- src/main/java/com/keto/graphqldemo/component/problemz/ItemSearchDataResolver.java
+++ src/main/java/com/keto/graphqldemo/component/problemz/ItemSearchDataResolver.java
@@ -2,22 +2,37 @@ package com.keto.graphqldemo.component.problemz;
 
 
 import com.keto.generated.DgsConstants;
+import com.keto.generated.types.Problem;
 import com.keto.generated.types.SearchItemFilter;
 import com.keto.generated.types.SearchableItem;
+import com.keto.generated.types.Solution;
+import com.keto.graphqldemo.service.query.ProblemQueryService;
+import com.keto.graphqldemo.service.query.SolutionQueryService;
+import com.keto.graphqldemo.util.GraphqlMapper;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
 import com.netflix.graphql.dgs.InputArgument;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * GraphQL resolver responsible for handling item search operations.
- *
+ * <p>
  * This resolver provides query operations to search for items
  * based on the specified search criteria.
  */
 @DgsComponent
 public class ItemSearchDataResolver {
+
+    @Autowired
+    private ProblemQueryService problemQueryService;
+
+    @Autowired
+    private SolutionQueryService solutionQueryService;
+
 
     /**
      * Searches for items that match the provided filter criteria.
@@ -32,8 +47,18 @@ public class ItemSearchDataResolver {
             @InputArgument(name = "filter", collectionType = SearchableItem.class)
             SearchItemFilter filter) {
 
-        // TODO: Search for items based on the provided filter criteria.
-        return null;
+        var result = new ArrayList<SearchableItem>();
+
+        List<Problem> problemList = problemQueryService.problemzByKeyword(filter.getKeyword())
+                .stream().map(GraphqlMapper::mapToGraphql).toList();
+        result.addAll(problemList);
+        List<Solution> solutionList = solutionQueryService.problemzByKeyword(filter.getKeyword())
+                .stream().map(GraphqlMapper::mapToGraphql).toList();
+
+        result.addAll(solutionList);
+
+        result.sort(Comparator.comparing(SearchableItem::getCreatedDate).reversed());
+        return result;
     }
 
 }
