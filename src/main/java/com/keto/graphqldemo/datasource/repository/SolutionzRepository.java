cc869cf3183b5cc5b572baed61744a16bc5ec9a3
kiransing <bhatkiran74@gmail.com>
2026-08-11 01:31:36 +0530

    feat: added findByKeyword SolutionzRepository method

--- src/main/java/com/keto/graphqldemo/datasource/repository/SolutionzRepository.java
+++ src/main/java/com/keto/graphqldemo/datasource/repository/SolutionzRepository.java
@@ -1,11 +1,17 @@
 package com.keto.graphqldemo.datasource.repository;
 
 import com.keto.graphqldemo.datasource.entity.Solutionz;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface SolutionzRepository extends CrudRepository<Solutionz, UUID> {
+
+    @Query(value = "Select * from solutionz where upper(content) like upper(:keyword)", nativeQuery = true)
+    List<Solutionz> findByKeyword(@Param("keyword") String keyword);
 }
