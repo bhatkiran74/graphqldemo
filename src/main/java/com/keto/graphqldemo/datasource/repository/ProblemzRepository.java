84cffc653c7b84c09de45a4962d0e2b4f31c8dcb
kiransing <bhatkiran74@gmail.com>
2026-08-11 01:31:26 +0530

    feat: added findByKeyword ProblemzRepository method

--- src/main/java/com/keto/graphqldemo/datasource/repository/ProblemzRepository.java
+++ src/main/java/com/keto/graphqldemo/datasource/repository/ProblemzRepository.java
@@ -1,7 +1,9 @@
 package com.keto.graphqldemo.datasource.repository;
 
 import com.keto.graphqldemo.datasource.entity.Problemz;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,10 @@ import java.util.UUID;
 @Repository
 public interface ProblemzRepository extends CrudRepository<Problemz, UUID> {
     List<Problemz> findAllByOrderByCreationTimestampDesc();
+
+    @Query(value = "Select * from problemz where upper(content) like upper(:keyword)" +
+            "or upper(title) like upper(:keyword)" +
+            "or upper(tags) like upper(:keyword)", nativeQuery = true)
+    List<Problemz> findByKeyword(@Param("keyword") String keyword);
+
 }
